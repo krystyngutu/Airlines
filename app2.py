3dca0a7ef77a75259ad9230b257d4f160a5fd3ed
Krystyn Gutu <102545277+krystyngutu@users.noreply.github.com>
2025-05-18 20:02:32 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -88,14 +88,20 @@ custom_colors = ['#d71920', '#00235f', '#f9ba00', '#660000', '#800080', '#3366ff
 # SIDEBAR FILTERS
 # ----------------------
 st.sidebar.header("Filters")
-direct_airlines = ['SWISS', 'United', 'Delta']
-lufthansa_group = ['Austrian', 'Brussels Airlines', 'Discover Airlines', 'Eurowings',
-                   'Edelweiss Air', 'ITA', 'Air Dolomiti', 'Lufthansa', 'SWISS']
-star_alliance = ['Aegean', 'Air Canada', 'Air China', 'Air India', 'Air New Zealand', 'ANA',
-                 'Asiana Airlines', 'Austrian', 'Avianca', 'Brussels Airlines', 'CopaAirlines',
-                 'Croatia Airlines', 'Egyptair', 'Ethiopian Airlines', 'Eva Air', 'LOT Polish Airlines',
-                 'Lufthansa', 'Shenzhen Airlines', 'Singapore Airlines', 'South African Airways',
-                 'SWISS', 'Tap Air Portugal', 'Thai', 'Turkish Airlines', 'United']
+direct_airlines = ['SWISS', 'Delta', 'United']
+
+lufthansa_group = [
+    'Austrian', 'Brussels Airline', 'Discover Airlines', 'Eurowings', 
+    'Edelweiss Air', 'ITA Airways', 'Air Dolomiti', 'Lufthansa'
+]
+
+star_alliance = [
+    'Aegean', 'Air Canada', 'Air China', 'Air India', 'Air New Zealand', 'ANA',
+    'Asiana Airlines', 'Austrian', 'Avianca', 'Brussels Airport', 'CopaAirlines',
+    'Croatia Airlines', 'Egyptair', 'Ethiopian Airlines', 'Eva Air', 'LOT Polish Airlines',
+    'Lufthansa', 'Shenzhen Airlines', 'Singapore Airlines', 'South African Airways',
+    'SWISS', 'TAP AirPortugal', 'Thai', 'Turkish Airlines', 'United'
+]
 
 group_option = st.sidebar.radio("Airline Group", ['All Airlines', 'Direct Airlines', 'Lufthansa Group', 'Star Alliance'])
 
@@ -111,6 +117,9 @@ else:
     airline_filter = all_airlines_in_data
 
 df_filtered = df[df['airline'].isin(airline_filter)]
+
+st.caption(f"Airlines included: {', '.join(sorted(df_filtered['airline'].unique()))}")
+
 
 
 if df_filtered['price'].dropna().empty:
